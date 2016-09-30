e1e496cc28515983c32baf35525e694a839792da
jennyf19 <v-jeferr@microsoft.com>
2016-09-30 10:21:15 -0700

    regex test

---
+++ Documents/Visual Studio 2015/Projects/parseTest2/parseTest2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace parsingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TagName = "v1.23.890";
+            TagNameFormatCheck(TagName);
+            Console.ReadLine();
+        }
+
+        public static string TagNameFormatCheck(string TagName)
+        {
+            Regex TagNamePattern = new Regex(@"^v[1-9]\.([0-9][0-9]\.)([0-9][0-9][0-9])$");
+
+            Console.WriteLine("The tag name is " + TagName);
+            Console.WriteLine("The tag name pattern is " + TagNamePattern);
+
+            if (Regex.IsMatch(TagName, TagNamePattern.ToString()) && TagName != null)
+            {
+                //TagName = TagNamePattern.ToString();
+                Console.WriteLine(TagName);
+                return TagName;
+            }
+
+            else
+            {
+                string formatError = "NO MATCH";
+                Console.WriteLine(formatError);
+                return formatError;
+            }
+
+        }
+    }
+}
